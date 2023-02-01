6df3062e202ba217a8eac3c138dd962462451f2c
Tom Byers <tmbyers1102@gmail.com>
2023-02-01 01:19:22 -0800

    end of video

--- backend/dashboard/settings.py
+++ backend/dashboard/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,8 +41,11 @@ INSTALLED_APPS = [
     # 3rd party API services
     'algoliasearch_django',
     # 3rd party packages
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
+
     # internal apps
     'api',
     'products',
@@ -52,6 +56,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -60,6 +65,19 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'dashboard.urls'
+CORS_URLS_REGEX = r"^/api/.*"
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8111',
+    'https://localhost:8111',
+]
+
+# if DEBUG:
+#     CORS_ALLOWED_ORIGINS += [
+#         'http://localhost:8111',
+#         'https://localhost:8111',
+#     ]
+
 
 TEMPLATES = [
     {
@@ -137,7 +155,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
-        "api.authentication.TokenAuthentication"
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "api.authentication.TokenAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
@@ -151,4 +170,10 @@ ALGOLIA = {
     'APPLICATION_ID': 'W46NITLZ9F',
     'API_KEY': 'a8c084281c635c5fc92fe964e524f2c3',
     'INDEX_PREFIX': 'fdx',
+}
+
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ["Bearer"],
+    "ACCESS_TOKEN_LIFETIME": datetime.timedelta(seconds=30), # minutes=5
+    "REFRESH_TOKEN_LIFETIME": datetime.timedelta(minutes=1), # days=1
 }
